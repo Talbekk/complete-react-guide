592edab904fcb4e59f6092690d062ee1b713ccdb
Talbekk <mt.campbell@outlook.com>
2022-07-26 22:08:17 +0100

    adds new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem/>
     </div>
   );
 }
