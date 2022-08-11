50887b80b6cf4a5e3c75765e017d517ded01276d
Talbekk <mt.campbell@outlook.com>
2022-08-11 16:42:11 +0100

    adds basic input form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/expenses/Expenses";
+import NewExpense from "./components/newExpense/NewExpense";
 
 const  App = () => {
   const expenses = [
@@ -28,7 +29,7 @@ const  App = () => {
   ];
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses expenses={expenses}/>
     </div>
   );
