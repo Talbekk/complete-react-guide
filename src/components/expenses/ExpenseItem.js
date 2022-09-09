f20b3019a3c7e6a58ed2ce1affcb4d47140c3745
Talbekk <mt.campbell@outlook.com>
2022-09-09 18:55:54 +0100

    refactors expenses into a rendered list

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -4,8 +4,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
